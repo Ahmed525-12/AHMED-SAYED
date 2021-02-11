9c58a4d7168fb8a3f04773001ca410e7e11c9208
Ahmed525-12 <70807060+Ahmed525-12@users.noreply.github.com>
2021-02-12 02:56:43 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -49,3 +49,54 @@ $(document).ready(function(){
         })
     })
 })
+
+// -----------------------------------------------------------------------------------------------------
+
+var sections = document.querySelectorAll("section");
+
+onscroll = function () {
+  var scrollPosition = document.documentElement.scrollTop;
+
+  sections.forEach((section) => {
+    if (
+      scrollPosition >= section.offsetTop - section.offsetHeight * 0.25 &&
+      scrollPosition <
+        section.offsetTop + section.offsetHeight - section.offsetHeight * 0.25
+    ) {
+      var currentId = section.attributes.id.value;
+      removeAllActiveClasses();
+      addActiveClass(currentId);
+    }
+  });
+};
+
+var removeAllActiveClasses = function () {
+  document.querySelectorAll("nav a").forEach((el) => {
+    el.classList.remove("activee");
+  });
+};
+
+var addActiveClass = function (id) {
+  // console.log(id);
+  var selector = `nav a[href="#${id}"]`;
+  document.querySelector(selector).classList.add("activee");
+};
+
+var navLinks = document.querySelectorAll("nav a");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    var currentId = e.target.attributes.href.value;
+    var section = document.querySelector(currentId);
+    var sectionPos = section.offsetTop;
+    section.scrollIntoView({
+      behavior: "smooth",
+    });
+
+    window.scroll({
+      top: sectionPos,
+      behavior: "smooth",
+    });
+  });
+});
