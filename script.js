ffd4faa2837a25cdaa9129bafe04c1cb16947bf2
Ahmed525-12 <70807060+Ahmed525-12@users.noreply.github.com>
2021-02-15 18:51:16 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ $("a[href^='#']").click(function(e) {
 $(document).ready(function(){
     
     $("#store-container").fadeOut(3000,function(){
-        $("#loading").fadeOut(2000,function(){
+        $("#loading").fadeOut(500,function(){
           
             $("#loading").remove()
         })
@@ -52,51 +52,6 @@ $(document).ready(function(){
 
 // -----------------------------------------------------------------------------------------------------
 
-var sections = document.querySelectorAll("section");
+$('body').scrollspy({target: '.navbar-fixed-top'})
 
-onscroll = function () {
-  var scrollPosition = document.documentElement.scrollTop;
-
-  sections.forEach((section) => {
-    if (
-      scrollPosition >= section.offsetTop - section.offsetHeight * 0.25 &&
-      scrollPosition <
-        section.offsetTop + section.offsetHeight - section.offsetHeight * 0.25
-    ) {
-      var currentId = section.attributes.id.value;
-      removeAllActiveClasses();
-      addActiveClass(currentId);
-    }
-  });
-};
-
-var removeAllActiveClasses = function () {
-  document.querySelectorAll("nav a").forEach((el) => {
-    el.classList.remove("activee");
-  });
-};
-
-var addActiveClass = function (id) {
-  // console.log(id);
-  var selector = `nav a[href="#${id}"]`;
-  document.querySelector(selector).classList.add("activee");
-};
-
-var navLinks = document.querySelectorAll("nav a");
-
-navLinks.forEach((link) => {
-  link.addEventListener("click", (e) => {
-    e.preventDefault();
-    var currentId = e.target.attributes.href.value;
-    var section = document.querySelector(currentId);
-    var sectionPos = section.offsetTop;
-    section.scrollIntoView({
-      behavior: "smooth",
-    });
-
-    window.scroll({
-      top: sectionPos,
-      behavior: "smooth",
-    });
-  });
-});
+// ---------------------------------------------------------------------------------------------------------
